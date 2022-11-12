551b532700fe04857c58e1f36864fa7d95d4c473
Alyona <alcom-5@yandex.ru>
2022-11-12 23:02:34 +0300

    made home work

---
+++ assets/js/js1_dz2,3.js
@@ -0,0 +1,18 @@
+let namePlanet = 'Earth';
+console.log(namePlanet);
+
+const books = ['book1', 'book2', 'book3', 'book4', 'book5'];
+console.log(books);
+
+let userName = 'User Name';
+console.log(userName);
+
+let admin = 'Джон';
+let name = 'Джон';
+alert(admin);
+
+
+
+
+
+
