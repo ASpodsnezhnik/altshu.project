8aede2b48b13402dd04aba19283632eda77a2ec2
Alyona <alcom-5@yandex.ru>
2022-12-03 18:48:49 +0300

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
-{
-    
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+
+navBtn.onclick = () => {
+   if (nav.classList.toggle('open')){
+
+   } 
 }
